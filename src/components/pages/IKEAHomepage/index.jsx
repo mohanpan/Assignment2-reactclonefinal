665f1155d561281facca894e46652a2c6c85ce2a
mohanpan <echoiepan@gmail.com>
2021-11-19 23:59:20 -0500

    sidebar

--- src/components/pages/IKEAHomepage/index.jsx
+++ src/components/pages/IKEAHomepage/index.jsx
@@ -1,9 +1,17 @@
 import "./styles.css";
+import * as FaIcons from "react-icons/fa";
 
 export const IKEAHomepage = () => {
   return (
-    <div className="home-page">
-      Ads
-    </div>
+    <>
+      <div className="home-page">
+        <Link to ="#" className='menu-bars'>
+          <FaIcons.FaBars />
+        </Link>
+      </div>
+      <nav className={sidebar ? 'nav-menu active' : 'nav-meanu'}>
+
+      </nav>
+    </>
   );
 };
